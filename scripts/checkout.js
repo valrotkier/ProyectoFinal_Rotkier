21c1dc6de889d04bd23e33b72240e262f19cef93
valrotkier <valrotkier@gmail.com>
2023-12-12 04:30:36 -0300

    Eliminando productos del HTML cuando hacemos click en eliminar

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -101,5 +102,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
